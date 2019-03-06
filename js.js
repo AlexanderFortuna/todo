3900d4d83ae908f5582fbbdd1b74708e333bbc75
AlexanderFortuna <32539463+AlexanderFortuna@users.noreply.github.com>
2019-03-06 18:03:14 +0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,36 @@
+javascript
+window.onload = function () {
+
+	var todoList = [];
+	if (localStorage.getItem("todo") !=undefined){
+		todoList = JSON.parse(localStorage.getItem("todo"));
+		out();
+	}
+
+	document.getElementById("add").onclick = function() {
+		var d = document.getElementById("in").value;
+		//{todo : Добавить пиво,check: false}
+		var temp{};
+		temp.todo = d;
+		temp.check = false;
+		var i = todoList.lenght;
+		todoList[i] = temp;
+		console.log(todoList);
+		out();
+		localStorage.setItem("todo",JSON.stringify(todoList) );
+	}
+	function out () {
+		var out="";
+		for (var key in todoList){
+			if(todoList[key].check == true){
+				out += "<input type='checkbox' checked>";
+			}
+			else {
+				out +=<input type='checkbox' >";
+			}
+			out += todoList[key].todo + "<br>";
+
+		}
+		document.getElementById("out").innerHTML = out;
+	}
+}
